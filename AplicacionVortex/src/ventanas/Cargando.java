d27535a1b1b12bc12447fc597acabe46db54f21c
admin <admin@Win7_1TB>
2017-05-19 11:36:35 +0300

    Cargando.java bueno

--- AplicacionVortex/src/ventanas/Cargando.java
+++ AplicacionVortex/src/ventanas/Cargando.java
@@ -14,6 +14,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.border.LineBorder;
 
 public class Cargando extends JFrame {
 	
@@ -44,30 +45,30 @@ public class Cargando extends JFrame {
 	 * Create the frame.
 	 */
 	public Cargando() {
-		setTitle("Login de Duran");
+		setTitle("Vortex");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 400, 400);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.BLACK);
+		contentPane.setBackground(new Color(102, 153, 204));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Continuar");
-		btnNewButton.setFont(new Font("Dialog", Font.PLAIN, 12));
-		btnNewButton.setForeground(Color.RED);
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 12));
+		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.setBounds(144, 230, 111, 29);
 		contentPane.add(btnNewButton);
 		
-		JLabel lblCargando = new JLabel("Bienvenido!");
-		lblCargando.setFont(new Font("Dialog", Font.PLAIN, 14));
+		JLabel lblCargando = new JLabel("Bienvenido:");
+		lblCargando.setFont(new Font("Dialog", Font.BOLD, 14));
 		lblCargando.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCargando.setForeground(Color.WHITE);
-		lblCargando.setBounds(144, 148, 111, 38);
+		lblCargando.setBounds(75, 155, 111, 29);
 		contentPane.add(lblCargando);
 		
 		JButton btnCerrarSesion = new JButton("Cerrar Sesion");
-		btnCerrarSesion.setFont(new Font("Dialog", Font.PLAIN, 13));
+		btnCerrarSesion.setFont(new Font("Dialog", Font.BOLD, 12));
 		btnCerrarSesion.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
@@ -85,9 +86,24 @@ public class Cargando extends JFrame {
 				dispose();
 			}
 		});
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		lblNewLabel.setOpaque(true);
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 14));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(190, 159, 111, 23);
+		contentPane.add(lblNewLabel);
 		btnCerrarSesion.setForeground(Color.RED);
-		btnCerrarSesion.setBounds(253, 11, 129, 23);
+		btnCerrarSesion.setBounds(269, 11, 113, 23);
 		contentPane.add(btnCerrarSesion);
+		
+		JLabel lblVortex = new JLabel("Vortex\u2122");
+		lblVortex.setForeground(Color.RED);
+		lblVortex.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblVortex.setBounds(315, 339, 67, 23);
+		contentPane.add(lblVortex);
 		
 	}
 }
