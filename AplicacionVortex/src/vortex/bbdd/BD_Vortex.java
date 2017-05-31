c759f028e3989924e8f1edc0b8127d3152edaa12
admin <admin@Win7_1TB>
2017-05-31 11:06:35 +0300

    Comentarios en BD_Vortex -> Terminado

--- AplicacionVortex/src/vortex/bbdd/BD_Vortex.java
+++ AplicacionVortex/src/vortex/bbdd/BD_Vortex.java
@@ -20,7 +20,11 @@ public class BD_Vortex extends BD_Conector {
 		super(file);
 	}
 
-	// metodo que devuelve un vector con todos los socios del cibercafe
+	/**
+	 * metodo que devuelve un vector con todos los socios del cibercafe
+	 * 
+	 * @return Vector<Socio> socio
+	 */
 	public Vector<Socio> ver_socios() {
 		String cadenaSQL = "Select * from socio";
 		Vector<Socio> socio = new Vector<Socio>();
@@ -48,17 +52,19 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para Añadir Socios
-	public int anadir_Socio(Socio socio) {
-		// ID_Socio DNI Nombre Direccion Telefono Correo Nick
-		// Password Premium FH_Conexion Fecha_alta Bono Fecha_baja
+	/**
+	 * metodo para Añadir Socios a la BBDD
+	 * 
+	 * @param socio
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 
+	public int anadir_Socio(Socio socio) {
 		String cadenaSQL = "INSERT INTO socio (DNI, Nombre, Direccion, Telefono, Correo, Nick,Password ,Premium ,FH_Conexion, Fecha_alta, Bono, Fecha_baja) VALUES('"
 				+ socio.getDni() + "','" + socio.getNombre() + "','" + socio.getDireccion() + "','"
 				+ socio.getTelefono() + "','" + socio.getCorreo() + "','" + socio.getUsuario() + "','"
 				+ socio.getPassword() + "','" + socio.getPremium() + "','" + socio.getFH_Conexion() + "','"
 				+ socio.getFechaAlta() + "','" + socio.getBono() + "','" + socio.getFechaBaja() + "')";
-		// System.out.println(cadenaSQL);
 		try {
 			this.abrir();
 			s = c.createStatement();
@@ -71,7 +77,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para iniciar sesion como Socio
+	/**
+	 * metodo para iniciar sesion como Socio
+	 * 
+	 * @param usu
+	 * @param password
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int loginSocio(String usu, String password) {
 
 		String cadenaSQL = "Select * from socio where nick='" + usu + "' and password='" + password + "'";
@@ -90,7 +102,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para iniciar sesion como Administrador
+	/**
+	 * metodo para iniciar sesion como Administrador
+	 * 
+	 * @param usu
+	 * @param password
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int loginAdmin(String usu, String password) {
 
 		String cadenaSQL = "Select * from administrador where Admin_user='" + usu + "' and Admin_password='" + password
@@ -110,7 +128,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para iniciar sesion como dependiente
+	/**
+	 * metodo para iniciar sesion como Dependiente
+	 * 
+	 * @param usu
+	 * @param password
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int loginDependiente(String usu, String password) {
 
 		String cadenaSQL = "Select * from dependiente where usuario='" + usu + "' and password='" + password + "'";
@@ -129,7 +153,14 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para cambiar la contraseña desde el panel de Socios
+	/**
+	 * metodo para cambiar la contraseña desde el panel de Socios
+	 * 
+	 * @param password
+	 * @param passwordNueva1
+	 * @param passwordNueva2
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int updatePassSocio(String password, String passwordNueva1, String passwordNueva2) {
 		String cadenaSQL = "Select * from socio where nick='" + Constantes.user + "' and password='" + password + "'";
 		// System.out.println(cadenaSQL);
@@ -159,10 +190,15 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	public int bajaSocio(String usu) {
+	/**
+	 * metodo paradar de baja a un Socio
+	 * 
+	 * @param usu
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 
+	public int bajaSocio(String usu) {
 		String cadenaSQL = "delete from socio where nick='" + usu + "'";
-		// System.out.println(cadenaSQL);
 		try {
 			this.abrir();
 			s = c.createStatement();
@@ -175,6 +211,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para añadir minutos a un socio.
+	 * 
+	 * @param usu
+	 * @param minutos
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int darConexion(String usu, int minutos) {
 		BD_Vortex bd = new BD_Vortex("mysql-properties.xml");
 
@@ -193,6 +236,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para actualizar los minutos disponibles del socio al cerrar sesion
+	 * 
+	 * @param usu
+	 * @param minutos
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int guardarConexion(String usu, int minutos) {
 		String cadenaSQL = "update socio set minutos=" + Constantes.minutos + " where nick='" + usu + "'";
 		// System.out.println(cadenaSQL);
@@ -208,6 +258,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para cambiar el nombre de usuario de un socio
+	 * 
+	 * @param usu
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int updateNick(String usu) {
 		String cadenaSQL = "update socio set nick='" + usu + "' where nick='" + Constantes.user + "'";
 		// System.out.println(cadenaSQL);
@@ -223,7 +279,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
-	// metodo para pasar los minutos
+	/**
+	 * metodo para pasar los minutos
+	 * 
+	 * @param nick
+	 * @return minutos disponibles del socio
+	 */
 	public int getMinutoSocio(String nick) {
 		int minutos = 0;
 		String cadenaSQL = "Select minutos from socio where nick='" + nick + "'";
@@ -243,6 +304,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para ver la ultima conexion de un socio
+	 * 
+	 * @param usu
+	 * @return ultima fecha de conexion
+	 */
 	public LocalDate ver_ultimaConexion(String usu) {
 		String cadenaSQL = "Select FH_Conexion from socio where nick='" + usu + "'";
 		try {
@@ -262,6 +329,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para ver los minutos disponibles de un socio
+	 * 
+	 * @param usu
+	 * @return minutos dispobnibles
+	 */
 	public int ver_Tiempos(String usu) {
 		String cadenaSQL = "Select minutos from socio where nick='" + usu + "'";
 		try {
@@ -280,6 +353,11 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo que devuelve los productos de la bbdd
+	 * 
+	 * @return Vector<Productos> productos
+	 */
 	public Vector<Productos> ver_Productos() {
 		String cadenaSQL = "Select * from productos";
 		Vector<Productos> productos = new Vector<Productos>();
@@ -299,6 +377,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para cobrar a un socio por un producto
+	 * 
+	 * @param producto
+	 * @param cantidad1
+	 * @return precio total
+	 */
 	public double cobrar(String producto, double cantidad1) {
 		String cadenaSQL = "Select precio from productos where nombre='" + producto + "'";
 		try {
@@ -318,6 +403,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo que devuelve el ID_Socio de la bbdd
+	 * 
+	 * @param usu
+	 * @return id
+	 */
 	public int verCodSocio(String usu) {
 		String cadenaSQL = "Select ID_Socio from socio where nick='" + usu + "'";
 
@@ -337,6 +428,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * metodo para devolver el codigo de un producto de la BBDD
+	 * 
+	 * @param nombre
+	 * @return id
+	 */
 	public int verCodProducto(String nombre) {
 		String cadenaSQL = "Select ID_Producto from productos where nombre='" + nombre + "'";
 
@@ -356,6 +453,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para ver la cantidad total de un producto en la BBDD
+	 * 
+	 * @param nombre
+	 * @return cantidad disponible
+	 */
 	public int verStock(String nombre) {
 		String cadenaSQL = "Select cantidad from productos where nombre='" + nombre + "'";
 
@@ -375,6 +478,14 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo que añade un registro en la tabla compras
+	 * 
+	 * @param idSocio
+	 * @param idProducto
+	 * @param cantidad
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int anadirCompra(int idSocio, int idProducto, int cantidad) {
 		String cadenaSQL = "insert into compras values(null," + idSocio + "," + idProducto + "," + cantidad + ")";
 
@@ -390,6 +501,13 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo para restar los productos de la bbdd cuando se hace una compra
+	 * 
+	 * @param idProducto
+	 * @param cantidad
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int restarProductos(int idProducto, int cantidad) {
 		String cadenaSQL = "select cantidad from productos where id_Producto=" + idProducto;
 
@@ -403,11 +521,11 @@ public class BD_Vortex extends BD_Conector {
 			}
 			int numDisponibles = numProductos - cantidad;
 			String SQL = "update productos set cantidad=" + numDisponibles + " where id_Producto=" + idProducto;
-
+			int filas = -1;
 			try {
 				this.abrir();
 				s = c.createStatement();
-				int filas = s.executeUpdate(SQL);
+				filas = s.executeUpdate(SQL);
 
 			} catch (SQLException e) {
 				return -1;
@@ -415,12 +533,19 @@ public class BD_Vortex extends BD_Conector {
 
 			s.close();
 			this.cerrar();
-			return 1;
+			return filas;
 		} catch (SQLException e) {
 			return -2;
 		}
 	}
 
+	/**
+	 * Metodo que añade mas productos a los que tenemos en la BBDD
+	 * 
+	 * @param idProducto
+	 * @param cantidad
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int pedirProductos(int idProducto, int cantidad) {
 		String cadenaSQL = "select cantidad from productos where id_Producto=" + idProducto;
 
@@ -452,6 +577,12 @@ public class BD_Vortex extends BD_Conector {
 		}
 	}
 
+	/**
+	 * Metodo que crea un nuevo producto a la tabla productos
+	 * 
+	 * @param producto
+	 * @return filas afectadas en la BBDD; -1 si se produce excepcion
+	 */
 	public int anadir_Producto(Productos producto) {
 		String cadenaSQL = "INSERT INTO productos (Nombre, Tipo, Precio, Cantidad) VALUES('" + producto.getNombre()
 				+ "', '" + producto.getTipo() + "'," + producto.getPrecio() + "," + producto.getCantidad() + ");";
